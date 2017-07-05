8d6e7cb453389e669f55db52479814dbdd16d77b
Android-罗志敏 <54543534@domain.com>
2017-07-05 10:04:04 +0800

    1111111111111111111111111

--- app/src/main/java/com/kotlindemo/Main2Activity.kt
+++ app/src/main/java/com/kotlindemo/Main2Activity.kt
@@ -8,5 +8,36 @@ class Main2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        uni()
     }
+
+    fun sum(dd:Int,jj:Int): Int{
+        var a=4
+        var b :Int
+        b=9
+        val max = if (a > b){
+            print("Choose a")
+        }
+        else{
+            print("Choose b")
+        }
+
+        var  x=1
+        when (x) {
+            1 -> print("x == 1")
+            2 -> print("x == 2")
+            else -> { // 注意这个语句块
+                print("x is neither 1 nor 2")
+            }
+        }
+      return  dd+jj
+    }
+
+    fun uni():Unit{
+        val items = listOf("apple", "banana", "kiwi")
+        for (item in items) {
+            println(item)
+        }
+    }
+
 }
