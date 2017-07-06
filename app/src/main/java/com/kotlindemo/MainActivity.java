71aa82f44d75b5108ee7968afda3be872b17554e
Android-罗志敏 <54543534@domain.com>
2017-07-06 13:55:59 +0800

    recyclerview helper

--- app/src/main/java/com/kotlindemo/MainActivity.java
+++ app/src/main/java/com/kotlindemo/MainActivity.java
@@ -2,10 +2,20 @@ package com.kotlindemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.util.Log;
 
-public class MainActivity extends BaseNewDemo {
+import com.lvr.library.adapter.CommonAdapter;
+import com.lvr.library.holder.BaseViewHolder;
+import com.lvr.library.recyclerview.HRecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends BaseNewDemo {
+    HRecyclerView hRecyclerView;
+    List<String> mDatas=new ArrayList<>();
     @Override
     public void show() {
         Log.d("NewDemo","show");
@@ -19,6 +29,25 @@ public class MainActivity extends BaseNewDemo {
         NewDemo newDemo = new NewDemo();
         newDemo.show();
         newDemo.TO();
+
+         hRecyclerView= (HRecyclerView) findViewById(R.id.list);
+        mDatas.add("uuuuuuuuu");
+        mDatas.add("rrrrrrrrr");
+        mDatas.add("uuuuutttt");
+        mDatas.add("kkkkggggg");
+        mDatas.add("xxxxxxxxx");
+
+
+
+        CommonAdapter<String> mAdapter = new CommonAdapter<String>(this, R.layout.item_layout, mDatas) {
+            @Override
+            public void convert(BaseViewHolder holder, int position) {
+                holder.setText(R.id.tv_content,mDatas.get(position));
+
+            }
+        };
+        hRecyclerView.setAdapter(mAdapter);
+        hRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
